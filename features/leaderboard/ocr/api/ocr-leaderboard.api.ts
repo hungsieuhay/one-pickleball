fa25e5e5b83ce9712e04fd00be5557cd891b0099
ntdat1306 <ntdat1306@gmail.com>
2025-12-12 23:32:16 +0700

    feat: User elo api

--- features/leaderboard/ocr/api/ocr-leaderboard.api.ts
+++ features/leaderboard/ocr/api/ocr-leaderboard.api.ts
@@ -1,10 +1,13 @@
 import { fetchWrapper } from '@/utils/fetch.utils';
 
-import { OCRLeaderboardResponse } from '../../shared/types';
+import { OCRLeaderboardResponse, OCRUserEloResponse } from '../../shared/types';
 
 const OCRLeaderboardAPI = {
   getAll: (filter: string) => {
     return fetchWrapper<OCRLeaderboardResponse>(`/ocr/leaderboard?${filter}`);
+  },
+  getUserElo: (id: number) => {
+    return fetchWrapper<OCRUserEloResponse>(`/ocr/users/${id}/elo`);
   },
 };
 
